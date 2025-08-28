fa30131946c0f8a646bf733d34fef34ae4bc84b4
Darwin Koirala <darwinkoirala123@gmail.com>
2025-08-28 16:58:06 +0545

    fix: fixed padding in scaffold not applied in event info

--- app/src/main/java/com/batman/ecms/features/main/presentation/screens/EventInfoScreen.kt
+++ app/src/main/java/com/batman/ecms/features/main/presentation/screens/EventInfoScreen.kt
@@ -81,7 +81,6 @@ import java.util.Calendar
 
 @OptIn(ExperimentalMaterial3Api::class)
 @RequiresApi(Build.VERSION_CODES.O)
-@SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
 @Composable
 fun EventInfoScreen(
     eventId: String?,
@@ -127,112 +126,115 @@ fun EventInfoScreen(
                 Icon(Icons.Default.Add, contentDescription = "Add")
             }
         },
-        topBar = { NavBackTopAppBar("Event Info",navBack=navBack) }
-    ) {
-        when (val value = state.value) {
-            is UiState.Error -> {
-                Box(modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
-                    Text(value.message)
+        topBar = { NavBackTopAppBar("Event Info", navBack = navBack) }
+    ) { innerPadding ->
+        Box(modifier = Modifier.padding(innerPadding)) {
+            when (val value = state.value) {
+                is UiState.Error -> {
+                    Box(modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
+                        Text(value.message)
+                    }
+                    return@Scaffold
                 }
-                return@Scaffold
-            }
 
-            UiState.Loading -> {
-                CustomLoader()
-                return@Scaffold
-            }
+                UiState.Loading -> {
+                    CustomLoader()
+                    return@Scaffold
+                }
 
-            is UiState.Success<EventInfoData> -> {
-                Column(
-                    modifier = Modifier
-                        .fillMaxSize()
-                        .padding(horizontal = 16.dp)
-                        .padding(top = 16.dp)
-                ) {
+                is UiState.Success<EventInfoData> -> {
                     Column(
                         modifier = Modifier
                             .fillMaxSize()
-                            .verticalScroll(rememberScrollState())
+                            .padding(horizontal = 16.dp)
                     ) {
-                        AsyncImage(
-                            model = "https://images.unsplash.com/photo-1531058020387-3be344556be6?w=500",
-                            contentDescription = "",
+                        Column(
                             modifier = Modifier
-                                .fillMaxWidth()
-                                .height(200.dp)
-                                .clip(RoundedCornerShape(12.dp)),
-                            contentScale = ContentScale.Crop
-                        )
-
-                        Spacer(modifier = Modifier.height(16.dp))
-
-                        Column() {
-                            Text(
-                                text = value.data.eventName,
-                                fontSize = 22.sp,
-                                fontWeight = FontWeight.Bold
-                            )
-                            Text(
-                                text = value.data.location,
-                                fontSize = 16.sp,
-                                color = Color.Gray
-                            )
-                        }
-
-                        Spacer(modifier = Modifier.height(16.dp))
-
-                        Card(
-                            modifier = Modifier
-                                .fillMaxWidth(),
-                            shape = RoundedCornerShape(12.dp),
-                            elevation = CardDefaults.cardElevation(4.dp)
+                                .fillMaxSize()
+                                .verticalScroll(rememberScrollState())
                         ) {
-                            Column(
-                                modifier = Modifier
-                                    .background(MaterialTheme.colorScheme.surfaceContainer)
-                                    .padding(16.dp),
-                            ) {
-                                StatRow(
-                                    label = "Total Attendees",
-                                    value = value.data.attendees.toString()
-                                )
-                                StatRow(
-                                    label = "Total Activity",
-                                    value = value.data.activities.size.toString()
-                                )
-                                StatRow(
-                                    label = "Total Staffs",
-                                    value = "1"
-                                )
-                                value.data.staffCode?.let {
-                                    StatRow(
-                                        label = "Invite Code",
-                                        value = value.data.staffCode
-                                    )
-                                }
-
-                            }
-                        }
-
-                        Spacer(modifier = Modifier.height(24.dp))
-
-
-                        Text("Activities", fontWeight = FontWeight.Bold, fontSize = 18.sp)
-                        value.data.activities.forEach {
-                            ActivityStats(
-                                title = it.title,
-                                startTime = it.startTime,
-                                endTime = it.endTime,
-                                totalGuests = it.total,
-                                scannedGuests = it.checkedIn,
-                                onClick = { navigateToScan(it.activityId) },
-                                onScanClicked = { navigateToActivityCheckIn(it.activityId) }
-                            )
                             Spacer(modifier = Modifier.height(10.dp))
+                            AsyncImage(
+                                model = "https://images.unsplash.com/photo-1531058020387-3be344556be6?w=500",
+                                contentDescription = "",
+                                modifier = Modifier
+                                    .fillMaxWidth()
+                                    .height(200.dp)
+                                    .clip(RoundedCornerShape(12.dp)),
+                                contentScale = ContentScale.Crop
+                            )
+
+                            Spacer(modifier = Modifier.height(16.dp))
+
+                            Column() {
+                                Text(
+                                    text = value.data.eventName,
+                                    fontSize = 22.sp,
+                                    fontWeight = FontWeight.Bold
+                                )
+                                Text(
+                                    text = value.data.location,
+                                    fontSize = 16.sp,
+                                    color = Color.Gray
+                                )
+                            }
+
+                            Spacer(modifier = Modifier.height(16.dp))
+
+                            Card(
+                                modifier = Modifier
+                                    .fillMaxWidth(),
+                                shape = RoundedCornerShape(12.dp),
+                                elevation = CardDefaults.cardElevation(4.dp)
+                            ) {
+                                Column(
+                                    modifier = Modifier
+                                        .background(MaterialTheme.colorScheme.surfaceContainer)
+                                        .padding(16.dp),
+                                ) {
+                                    StatRow(
+                                        label = "Total Attendees",
+                                        value = value.data.attendees.toString()
+                                    )
+                                    StatRow(
+                                        label = "Total Activity",
+                                        value = value.data.activities.size.toString()
+                                    )
+                                    StatRow(
+                                        label = "Total Staffs",
+                                        value = "1"
+                                    )
+                                    value.data.staffCode?.let {
+                                        StatRow(
+                                            label = "Invite Code",
+                                            value = value.data.staffCode
+                                        )
+                                    }
+
+                                }
+                            }
+
+                            Spacer(modifier = Modifier.height(24.dp))
+
+
+                            Text("Activities", fontWeight = FontWeight.Bold, fontSize = 18.sp)
+                            value.data.activities.forEach {
+                                ActivityStats(
+                                    title = it.title,
+                                    startTime = it.startTime,
+                                    endTime = it.endTime,
+                                    totalGuests = it.total,
+                                    scannedGuests = it.checkedIn,
+                                    onClick = { navigateToScan(it.activityId) },
+                                    onScanClicked = { navigateToActivityCheckIn(it.activityId) }
+                                )
+                                Spacer(modifier = Modifier.height(10.dp))
+                            }
                         }
                     }
                 }
             }
+
         }
 
     }
